75b70162ddc8d59d76084398ec4b295be7f66b62
ahmettahaklc <ahmettahaklc@gmail.com>
2025-03-22 23:56:08 +0300

    Add database connection and required packages

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const express = require('express');
 // Second Method
 //import express from 'express'
 
+const {engine} = require('express-handlebars')
+const expressSession = require('express-session')
+const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+const path = require('path')
+const dbs = require(path.join(__dirname, 'dbs.js'))
+
+//DB Connect
+
+dbs()
