39da613ae9152808be696c27dd9b9c2810e5e4eb
ahmettahaklc <ahmettahaklc@gmail.com>
2025-03-23 22:19:39 +0300

    Set up Express server with handlebars, middleware and static files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // First Method: CommonJS
 const express = require('express');
 
+
 // Second Method
 //import express from 'express'
+
 
 const {engine} = require('express-handlebars')
 const expressSession = require('express-session')
@@ -11,6 +13,39 @@ const dotenv = require('dotenv')
 const path = require('path')
 const dbs = require(path.join(__dirname, 'dbs.js'))
 
-//DB Connect
 
+//DB Connect
 dbs()
+
+
+//Start Settings
+dotenv.config()
+const app = express()
+
+
+//Variables
+const time = 1000 * 60 * 30
+
+
+//The area where template engine is located
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
+
+
+//Middleware
+app.use(express.json())
+app.use(fileUpload())
+app.use(expressSession({
+    secret:'myBlog',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{path:'/',httpOnly:true,secure:false,maxAge:time}
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.listen(5000, ()=>{
+    console.log(`Server is running http://127.0.0.1:5000`)
+})
+
