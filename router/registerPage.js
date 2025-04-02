2487f308b8e9cd896507f9460a6f9b0f049c31d9
ahmettahaklc <ahmettahaklc@gmail.com>
2025-04-02 05:31:57 +0300

    add NoSQL schema and administrator information

--- router/registerPage.js
+++ router/registerPage.js
@@ -1,11 +1,15 @@
+const { error } = require('console')
 const express = require('express')
 const router = express.Router()
+const {join} = require('path')
+const { json } = require('stream/consumers')
+const User = require(join(__dirname, '..', 'model', 'userModel.js'))
 
 router.get('/', (req, res)=>{
     res.render('site/register')
 })
 
-router.post('/', (req, res)=>{
+router.post('/', async(req, res)=>{
     try {
         if(!req.body){
             return res.json({
@@ -23,9 +27,46 @@ router.post('/', (req, res)=>{
             })
         }
 
-        return res.json({
-            case: true,
-            message: 'Transaction is successfull'
+        const usernameRegex = new RegExp("^[a-zA-Z0-9]{1,12}$");
+        if (!usernameRegex.test(username)) {
+            return res.json({
+                case: false,
+                message: 'Invalid username! Must be max 12 characters and contain only letters and numbers.'
+            });
+        }
+
+        if (password.length < 8 || password.length > 15) {
+            return res.json({
+                case: false,
+                message: 'Invalid password! Must be between 8 and 15 characters.'
+            });
+        }
+
+        const userControl = await User.find({'username':username}).exec()   
+
+        if(userControl.length!=0){
+            return res.json({
+                case:false,
+                message: 'this email has been taken!'     
+            })    
+        }
+
+        const user = new User({
+            'username': username,
+            'password': password
+        })
+
+        user.save().then(()=>{
+            return  res.json({
+                case: true,
+                message: 'Registration is successfull'
+            })
+        }).catch((err)=>{
+            console.log(err)
+            return res.json({
+                case: false,
+                message: 'Unexpacted error'
+            })
         })
 
     } catch (error) {
