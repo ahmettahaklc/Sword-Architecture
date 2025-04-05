e5f51657f7a094479e18321f9d0c76c1a1253fc3
ahmettahaklc <ahmettahaklc@gmail.com>
2025-04-05 22:18:55 +0300

    implement login functionality with form handling and server-side integration

--- router/loginPage.js
+++ router/loginPage.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const router = express.Router()
+const {join} = require('path')
+const User = require(join(__dirname, '..', 'model', 'userModel.js'))
 
 router.get('/', (req, res)=>{
     if(res.locals.user){
         return res.redirect('/error')
     }
     res.render('site/login')
+})
+
+router.post('/', async(req, res)=>{
+    try {
+        if(res.locals.user){
+            return res.json({
+                case: false,
+                message: 'User is already logged.'
+            })        
+        }
+        
+        let  {username, password} = req.body
+        
+        const userControl = await User.find({'username':username, 'password':password}).exec()
+
+        console.log(username, password)
+
+        console.log(userControl)
+
+        if(userControl.length != 1){
+            return res.json({
+                case: false,
+                message: 'username or password is not correct'
+            })
+        }
+
+        let ID = userControl[0]._id
+        ID = String(ID)
+        req.session.userID = ID 
+
+        return res.json({
+            case: true,
+            message: 'login process is succesfull you are redirected'
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.json({
+            case: false,
+            message: 'An unexpected error occured.'
+        })
+    }
 })
 
 module.exports = router
