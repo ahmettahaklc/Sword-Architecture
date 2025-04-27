2aef372ff3c8024d852e24db9bf1a7037aad8684
ahmettahaklc <ahmettahaklc@gmail.com>
2025-04-27 19:31:29 +0300

    Add project creation feature with file upload and database integration

--- router/addPage.js
+++ router/addPage.js
@@ -1,5 +1,8 @@
+const { error } = require('console')
 const express = require('express')
 const router = express.Router()
+const{join} = require('path')
+const Content = require(join(__dirname, '..', 'model', 'contentModel.js'))
 
 router.get('/', (req, res)=>{
     if(!res.locals.user){
@@ -41,8 +44,38 @@ router.post('/', (req, res)=>{
             })    
         }
 
-        if(file.mimetype == 'img/jpeg' || file.mimetype == 'img/png' || file.mimetype == 'img/jpg'){
+        if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/jpg'){
+            const extension = file.mimetype.split('/')[1]
+            const uniqueName = `${Date.now()}-${Math.round(Math.random()*1E9)}.${extension}`
+            const pathName = join(__dirname, '..', 'public', 'images', 'content', uniqueName)
+            file.mv(pathName, (err)=>{
+                if(err!=undefined){
+                    return res.json({
+                        case: false,
+                        message: 'File could not added' 
+                    })
+                }
+                else{
+                    const db = new Content({
+                        title,
+                        content,
+                        'path': pathName 
+                    })
 
+                    db.save().then(()=>{
+                        return res.json({
+                            case: true,
+                            message: 'Data added successfully'
+                        })
+                    }).catch(err=>{
+                        console.log(err)
+                        return res.json({
+                            case: false,
+                            message: 'A error occured while saving to DB'
+                        })
+                    })
+                }    
+            }) 
         }
         else{
             return res.json({
@@ -50,12 +83,7 @@ router.post('/', (req, res)=>{
                 message: 'File type is not correct'
             })    
         }
-        
-        return res.json({
-            case: true,
-            message: 'Process continues'
-        })
-
+    
     } catch (error) {
         console.log(error)
         return res.json({
